3dad3a9415ddb29d4ba23893ad18769b8aff83da
Prasad Ingole <10679929+prasadingole@users.noreply.github.com>
2022-07-05 15:16:20 +0530

    Code Refactoring

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -1,5 +1,6 @@
 import { useSelector , useDispatch } from 'react-redux';
 import classes from './Counter.module.css';
+import { INCREMENT, DECREMENT, TOGGLE, INCREASEBY5 } from '../store/actionConstants';
 
 const Counter = () => {
   const dispatch = useDispatch();
@@ -7,19 +8,19 @@ const Counter = () => {
   const showCounter = useSelector(state => state.showCounter);
   
   const toggleCounterHandler = () => {
-    dispatch({type: 'toggle'})
+    dispatch({type: TOGGLE})
   };
   
   const incrementHandler= ()=> {
-    dispatch({type : 'increment'})
+    dispatch({type :  INCREMENT})
   };
 
   const decrementHandler= ()=> {
-    dispatch({type : 'decrement'})
+    dispatch({type : DECREMENT })
   };
 
   const incrementHandlerBy5 = () => {
-    dispatch({type : 'incrementby5',amount : 5})
+    dispatch({type : INCREASEBY5,amount : 5})
   }
 
   return (
