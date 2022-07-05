0f23327ab7a63fb3ab07b425009f9108fa142793
Prasad Ingole <10679929+prasadingole@users.noreply.github.com>
2022-07-05 15:08:28 +0530

    Code refactoring

---
+++ src/store/actionConstants.js
@@ -0,0 +1,4 @@
+export const INCREMENT = 'increment'
+export const DECREMENT = 'decrement'
+export const TOGGLE = 'toggle'
+export const INCREASEBY5 = 'incrementby5'
--- src/store/index.js
+++ src/store/index.js
@@ -1,30 +1,17 @@
-import { createStore } from 'redux'
+import { createStore } from 'redux';
+import { INCREMENT, DECREMENT, TOGGLE, INCREASEBY5 } from './actionConstants';
 const initialState = {counter : 0, showCounter : true} ;
 
 // Create reducer
 const counterReducer = (state = initialState, action) => {
-    if (action.type === 'increment') {
-        return {
-            counter: state.counter + 1,
-            showCounter : state.showCounter
-        }
-    } else if ( action.type === 'decrement' ) {
-        return {
-            counter : state.counter - 1,
-            showCounter : state.showCounter
-        }
-    } else if ( action.type === 'toggle') {
-        return {
-            counter : state.counter,
-            showCounter : !state.showCounter 
-        };
+    if (action.type === INCREMENT) {
+        return {...state, counter: state.counter + 1}
+    } else if ( action.type === DECREMENT ) {
+        return {...state, counter : state.counter - 1}
+    } else if ( action.type === TOGGLE) {
+        return {...state, showCounter : !state.showCounter}
 
-    } else if ( action.type === 'incrementby5'){
-        /*return {
-            counter : state.counter + action.amount,
-            showCounter : !state.showCounter 
-        };*/
-
+    } else if ( action.type ===  INCREASEBY5){
         return { ...state , counter : state.counter + action.amount};
     }
     return state;
